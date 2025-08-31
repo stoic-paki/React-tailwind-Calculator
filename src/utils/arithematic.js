c5124a20b36d800739446ba7d828e91491aae60f
stoic-paki <taha.s200526@gmail.com>
2025-08-31 07:27:13 +0500

    Removing comments

--- src/utils/arithematic.js
+++ src/utils/arithematic.js
@@ -1,21 +1,3 @@
-// function that splits the string into a array and checks the operator.
-
-// export function stringSplitter(string_exp, val1, val2, valOp, incrementer) {
-//     try {
-//         incrementer = 0
-//         // Split by operator but keep it with capturing group
-//         let arr_exp = string_exp.split(/([+\-*/])/).filter(Boolean);
-//         val1 = Number(arr_exp[0])
-//         val2 = Number(arr_exp[2])
-//         valOp = arr_exp[1]
-//         incrementer++
-//         console.log(val1, val2, valOp)
-//         evaluator(val1, val2, valOp)
-//     } catch (error) {
-//         console.error(error)
-//     }
-// }
-
 export function stringSplitter(string_exp) {
   try {
     // Split into ["12", "+", "5"]
@@ -33,30 +15,6 @@ export function stringSplitter(string_exp) {
     console.error(error);
   }
 }
-
-
-// export function evaluator(x, y, operator) {
-//     try {
-//         switch (operator) {
-//             case '+':
-//                 console.log(`operator is + so result is ${x + y}`)
-//                 break;
-//             case '-':
-//                 console.log(`operator is + so result is ${x + y}`)
-//                 break;
-//             // ... more cases
-//             case '/':
-//                 console.log(`operator is + so result is ${x + y}`)
-//                 break;
-//             case '*':
-//                 console.log(`operator is + so result is ${x + y}`)
-//                 break;            
-//         }
-
-//     } catch (error) {
-//         console.error(error)
-//     }
-// }
 
 export function evaluator(x, y, operator) {
   try {
@@ -82,15 +40,4 @@ export function evaluator(x, y, operator) {
   } catch (error) {
     console.error(error);
   }
-}
-
-
-export function checkOp(doesHave,string_exp, val1, val2, valOp, incrementer ){
-    try {
-        if(doesHave){
-            stringSplitter(string_exp, val1, val2, valOp, incrementer)
-        }
-    } catch (error) {
-        console.error(error)
-    }
 }
